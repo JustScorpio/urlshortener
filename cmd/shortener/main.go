57cb62f299500a8796cc5aebb421331b2f22784d
Stanislaw <skorpiello@gmail.com>
2025-04-28 13:14:31 +0500

    Запуск двух серверов в разных горутинах без обработки одинаковых адресов

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -37,30 +38,32 @@ func run() error {
 	// Инициализация обработчиков
 	shURLHandler := handlers.NewShURLHandler(shURLService, flagShURLBaseAddr)
 
-	// Если адрес один - запускаем то и то на одном порту
+	// // Если адрес один - запускаем то и то на одном порту
 	// if flagShortenerAddr == flagShURLBaseAddr {
-	r := chi.NewRouter()
-	r.Get("/{token}", shURLHandler.GetFullURL)
-	r.Post("/", shURLHandler.ShortenURL)
-	return http.ListenAndServe(flagShortenerAddr, r)
+	// 	r := chi.NewRouter()
+	// 	r.Get("/{token}", shURLHandler.GetFullURL)
+	// 	r.Post("/", shURLHandler.ShortenURL)
+	// 	return http.ListenAndServe(flagShortenerAddr, r)
 	// }
 
-	// // Если разные - разные сервера для разных хэндлеров
-	// shURLBase := chi.NewRouter()
-	// shURLBase.Get("/{token}", shURLHandler.GetFullURL)
-	// fmt.Println("Running short-to-long redirect server on", flagShURLBaseAddr)
-	// err = http.ListenAndServe(flagShURLBaseAddr, shURLBase)
-	// if err != nil {
-	// 	return err
-	// }
+	// Если разные - разные сервера для разных хэндлеров
+	// Запускаем оба сервера в горутинах
+	errCh := make(chan error)
 
-	// shortener := chi.NewRouter()
-	// shortener.Post("/", shURLHandler.ShortenURL)
-	// fmt.Println("Running URL shortener on", flagShortenerAddr)
-	// err = http.ListenAndServe(flagShortenerAddr, shortener)
-	// if err != nil {
-	// 	return err
-	// }
+	go func() {
+		shURLBase := chi.NewRouter()
+		shURLBase.Get("/{token}", shURLHandler.GetFullURL)
+		fmt.Println("Running short-to-long redirect server on", flagShURLBaseAddr)
+		errCh <- http.ListenAndServe(flagShURLBaseAddr, shURLBase)
+	}()
 
-	// return nil
+	go func() {
+		shortener := chi.NewRouter()
+		shortener.Post("/", shURLHandler.ShortenURL)
+		fmt.Println("Running URL shortener on", flagShortenerAddr)
+		errCh <- http.ListenAndServe(flagShortenerAddr, shortener)
+	}()
+
+	// Блокируем основную горутину и обрабатываем ошибки
+	return <-errCh
 }
